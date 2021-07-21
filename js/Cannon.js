aa6f547c1a59506d3b8302bf04708e975ba068a7
rooneyL2620L <85018597+rooneyL2620L@users.noreply.github.com>
2021-07-21 14:01:33 +0530

    Add files via upload

---
+++ js/Cannon.js
@@ -0,0 +1,36 @@
+class Cannon{
+    constructor(x,y,width,height,angle){
+        this.x=x
+        this.y=y
+        this.width=width
+        this.height=height
+        this.angle=angle
+    }
+display(){
+    fill ("darkgrey")
+    push ()
+    translate (this.x,this.y)
+    rotate (this.angle)
+    rect (-10,-20,this.width,this.height)
+    pop ()
+    arc (this.x-30,this.y+90,140,200,PI,TWO_PI)
+    noFill()
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+}
